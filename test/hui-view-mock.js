8fa9ca8682c7df2d6963115943a197cf1a8da8b7
Gerben ten Hove <gerben.ten.hove@ing.nl>
2019-08-11 14:59:14 +0200

    Cleanup ha-config for power-wheel-card residue?

--- test/hui-view-mock.js
+++
@@ -1,8 +0,0 @@
-import { LitElement, html as litHtml, css as litCss } from "lit-element";
-
-LitElement.prototype.html = litHtml;
-LitElement.prototype.css = litCss;
-
-class HuiView extends LitElement {}
-
-customElements.define("hui-view", HuiView);
